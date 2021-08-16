35d744e42e39ab346e8dec78699745011f6c9688
shahramDBI <shahramdarbandi79@gmail.com>
2021-08-16 17:14:31 +0430

    Initial

---
+++ cmd/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shahramDBI/Go_Web_01/pkg/handlers"
+	"net/http"
+)
+
+const portNumber = "8080"
+// main is the main application function
+func main()  {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shahramDBI/Go_Web_01
+
+go 1.16
